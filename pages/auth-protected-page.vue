0e6303cbb9846134f7cbc13e7e5d104198b798ba
andrew2020wit <andrew2020wit@gmail.com>
2020-12-26 20:50:46 +0200

    get private route

--- pages/auth-protected-page.vue
+++ pages/auth-protected-page.vue
@@ -1,10 +1,25 @@
-<template lang="pug">
-  div
-    h1 AuthProtectedPage
+<template>
+  <div>
+    <h1>Auth protected</h1>
+    <button @click="getSome">get</button>
+    {{ x1 }}
+  </div>
 </template>
 
 <script>
 export default {
   middleware: 'auth',
+  data() {
+    return {
+      x1: '',
+    }
+  },
+  methods: {
+    getSome() {
+      this.$axios
+        .$get('http://localhost:3001/api/jwt-auth/test-private-route')
+        .then((value) => (this.x1 = value))
+    },
+  },
 }
 </script>
